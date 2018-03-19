d485f183c615ed3293021c8372a4fc900c3a5c29
jonasCr <33930352+jonasCr@users.noreply.github.com>
2018-03-19 08:32:38 +0100

    Initial Release

---
+++ js/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+
+	cajaSkills=$('#skills');
+	cajaIdiomas=$('#idiomas');
+
+	$(window).scroll(function() {
+		documentScroll=$(document).scrollTop()+700;
+		if (documentScroll > cajaSkills.offset().top) {
+			$('.progress-html').animate({width: '90%'}, 2000);
+			$('.progress-css').animate({width: '85%'}, 2000);
+			$('.progress-js').animate({width: '60%'}, 2000);
+			$('.progress-wp').animate({width: '40%'}, 2000);
+		}
+	});
+
+	$(window).scroll(function() {
+		documentScroll=$(document).scrollTop()+700;
+		if (documentScroll > cajaIdiomas.offset().top) {
+			$('.progress-fr').animate({width: '100%'}, 2000);
+			$('.progress-en').animate({width: '70%'}, 2000);
+			$('.progress-es').animate({width: '80%'}, 2000);
+		}
+	});
+
+
+
+});
