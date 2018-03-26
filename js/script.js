f0c1df3ebd2bf08e7fd43cd0ce0955c499c264ad
jonasCr <33930352+jonasCr@users.noreply.github.com>
2018-03-26 14:11:22 +0200

    bug fix JS

--- js/script.js
+++ js/script.js
@@ -10,18 +10,10 @@ $(document).ready(function() {
 			$('.progress-css').animate({width: '85%'}, 2000);
 			$('.progress-js').animate({width: '60%'}, 2000);
 			$('.progress-wp').animate({width: '40%'}, 2000);
-		}
-	});
-
-	$(window).scroll(function() {
-		documentScroll=$(document).scrollTop();
-		if (documentScroll > cajaIdiomas.offset().top) {
 			$('.progress-fr').animate({width: '100%'}, 2000);
 			$('.progress-en').animate({width: '70%'}, 2000);
 			$('.progress-es').animate({width: '80%'}, 2000);
 		}
 	});
-
-
 
 });
