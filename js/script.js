2d409c1e0b391587decfa7d279d8041ca4f628d7
Jonas Cruchon <jonascr.dev@gmail.com>
2019-11-20 21:04:09 +0100

    update curriculum

--- js/script.js
+++ js/script.js
@@ -9,7 +9,11 @@ $(document).ready(function() {
 			$('.progress-html').animate({width: '90%'}, 2000);
 			$('.progress-css').animate({width: '85%'}, 2000);
 			$('.progress-js').animate({width: '60%'}, 2000);
-			$('.progress-wp').animate({width: '40%'}, 2000);
+			$('.progress-angular').animate({width: '60%'}, 2000);
+			$('.progress-flutter').animate({width: '50%'}, 2000);
+			$('.progress-vuejs').animate({width: '40%'}, 2000);
+			$('.progress-c').animate({width: '40%'}, 2000);
+			$('.progress-dart').animate({width: '40%'}, 2000);
 			$('.progress-fr').animate({width: '100%'}, 2000);
 			$('.progress-en').animate({width: '70%'}, 2000);
 			$('.progress-es').animate({width: '80%'}, 2000);
