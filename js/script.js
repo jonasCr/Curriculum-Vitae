3e4d621127a2f2c8f0b3f14368ae5a4560e9573e
Jonas Cruchon <jonascr.dev@gmail.com>
2019-12-19 21:43:58 +0100

    add sql and typescript

--- js/script.js
+++ js/script.js
@@ -11,9 +11,11 @@ $(document).ready(function() {
 			$('.progress-js').animate({width: '60%'}, 2000);
 			$('.progress-angular').animate({width: '60%'}, 2000);
 			$('.progress-flutter').animate({width: '50%'}, 2000);
+			$('.progress-ts').animate({width: '60%'}, 2000);
 			$('.progress-vuejs').animate({width: '40%'}, 2000);
 			$('.progress-c').animate({width: '40%'}, 2000);
 			$('.progress-dart').animate({width: '40%'}, 2000);
+			$('.progress-sql').animate({width: '30%'}, 2000);
 			$('.progress-fr').animate({width: '100%'}, 2000);
 			$('.progress-en').animate({width: '70%'}, 2000);
 			$('.progress-es').animate({width: '80%'}, 2000);
